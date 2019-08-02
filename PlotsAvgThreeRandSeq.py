a285b0e59c076ceb1bbddc3a00ce06e1bd0311c6
Maria <mariapaquin@u.boisestate.edu>
2019-08-02 11:50:21 -0600

    cleanup

--- PlotsAvgThreeRandSeq.py
+++ PlotsAvgThreeRandSeq.py
@@ -5,38 +5,34 @@ from sklearn.linear_model import LinearRegression;
 from scipy.stats.stats import pearsonr
 
 fig, ax = plt.subplots(1, 2)
-# fig.tight_layout()
 fig.set_figheight(4.5)
 fig.set_figwidth(12)
 
 fig2, ax2 = plt.subplots(1, 2)
-# fig2.tight_layout()
 fig2.set_figheight(4)
 fig2.set_figwidth(8)
 
-# Read in data
-withGreen1 = pd.read_csv("data/random/A/with-green.csv")
-withoutGreen1 = pd.read_csv("data/random/A/without-green.csv")
-invocations1 = pd.read_csv("data/random/A/invocations.csv")
-cacheHits1 = pd.read_csv("data/random/A/cacheHits.csv")
+green1 = pd.read_csv("data/random/A/with-green.csv")
+noGreen1 = pd.read_csv("data/random/A/without-green.csv")
+invocations = pd.read_csv("data/random/A/invocations.csv")
+hits = pd.read_csv("data/random/A/cacheHits.csv")
 
-withGreen2 = pd.read_csv("data/random/B/with-green.csv")
-withoutGreen2 = pd.read_csv("data/random/B/without-green.csv")
+green2 = pd.read_csv("data/random/B/with-green.csv")
+noGreen2 = pd.read_csv("data/random/B/without-green.csv")
 
-withGreen3 = pd.read_csv("data/random/C/with-green.csv")
-withoutGreen3 = pd.read_csv("data/random/C/without-green.csv")
+green3 = pd.read_csv("data/random/C/with-green.csv")
+noGreen3 = pd.read_csv("data/random/C/without-green.csv")
 
-withGreen1 = withGreen1.join(invocations1)
-withGreen1 = withGreen1.join(cacheHits1)
+green1 = green1.join(invocations)
+green1 = green1.join(hits)
 
+df1 = pd.merge(noGreen1, green1, on=['project', 'package', 'class', 'method'])
 
-df1 = pd.merge(withoutGreen1, withGreen1, on=['project', 'package', 'class', 'method'])
+df1 = pd.merge(df1, green2, on=['project', 'package', 'class', 'method'])
+df1 = pd.merge(df1, noGreen2, on=['project', 'package', 'class', 'method'])
 
-df1 = pd.merge(df1, withGreen2, on=['project', 'package', 'class', 'method'])
-df1 = pd.merge(df1, withoutGreen2, on=['project', 'package', 'class', 'method'])
-
-df1 = pd.merge(df1, withGreen3, on=['project', 'package', 'class', 'method'])
-df1 = pd.merge(df1, withoutGreen3, on=['project', 'package', 'class', 'method'])
+df1 = pd.merge(df1, green3, on=['project', 'package', 'class', 'method'])
+df1 = pd.merge(df1, noGreen3, on=['project', 'package', 'class', 'method'])
 
 df1.rename(columns={'time':'timeA',
                     'time_green':'time_greenA',
@@ -51,7 +47,6 @@ df1 = df1[df1['invocations'] > 0]
 df1['avgTime'] = df1[['timeA', 'timeB', 'timeC']].mean(axis=1)
 df1['avgTime_green'] = df1[['time_greenA', 'time_greenB', 'time_greenC']].mean(axis=1)
 
-# print(df1)
 
 df1.reset_index(inplace=True)
 df1 = df1.drop(['index', 'timeA', 'timeB', 'timeC', 'time_greenA', 'time_greenB', 'time_greenC'], axis=1)
@@ -60,7 +55,6 @@ print(df1.groupby('class')['method'].nunique().size)
 #
 # Calculate time ratio
 df1['Ts'] = pd.to_numeric(df1['avgTime_green'])/pd.to_numeric(df1['avgTime'])
-# print('size of sequence 1 ', df1['Ts'] .size)
 
 x = pd.DataFrame(np.arange(0.0, df1['Ts'].size, 1))
 
@@ -73,7 +67,6 @@ ax[0].set_ylabel('Ts')
 ax[0].set_xlabel('Program #')
 ax[0].set_ylim([0, 1.2])
 # ax[0].plot(x, Y_pred, color='red')
-
 
 df1['Rs'] = pd.to_numeric(df1['cacheHits'])/pd.to_numeric(df1['invocations'])
 
